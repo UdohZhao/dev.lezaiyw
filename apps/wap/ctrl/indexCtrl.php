877ffb4c54f966426b75ba86c9c27b9c603cc793
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-11-10 14:41:35 +0800

    zhaojian

--- apps/wap/ctrl/indexCtrl.php
+++ apps/wap/ctrl/indexCtrl.php
@@ -2,11 +2,17 @@
 namespace apps\wap\ctrl;
 use core\lib\conf;
 use apps\wap\model\banner;
+use apps\wap\model\serviceCategory;
+use apps\wap\model\service;
 class indexCtrl extends baseCtrl{
   public $bdb;
+  public $scdb;
+  public $sdb;
   // 构造方法
   public function _auto(){
     $this->bdb = new banner();
+    $this->scdb = new serviceCategory();
+    $this->sdb = new service();
   }
 
   /**
@@ -19,6 +25,10 @@ class indexCtrl extends baseCtrl{
     {
       // 读取banner
       $data['bData'] = $this->bdb->getcRows(0);
+      // 读取服务类别
+      $data['scData'] = $this->scdb->getcRows(0);
+      // 读取热门推荐服务
+      $data['sData'] = $this->sdb->gethotRows(2);
       // assign
       $this->assign('data',$data);
       // display
---
+++ apps/wap/model/service.php
@@ -0,0 +1,33 @@
+<?php
+namespace apps\wap\model;
+use core\lib\model;
+class service extends model
+{
+  public $table = 'service';
+
+  /**
+   * 读取首页热门服务
+   */
+  public function gethotRows($hot_status)
+  {
+    // sql
+    $sql = "
+      SELECT
+              *
+      FROM
+              `$this->table`
+      WHERE
+              1 = 1
+      AND
+              hot_status = '$hot_status'
+      AND
+              type = '2'
+      AND
+              status = '1'
+      ORDER BY
+              ctime DESC
+    ";
+    return $this->query($sql)->fetchAll(2);
+  }
+
+}
