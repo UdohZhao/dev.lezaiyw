3862b3ca16b0d17ee7210ea2c7a8bf81d306507e
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-11-10 18:41:23 +0800

    zhaojian

--- apps/wap/ctrl/meetCtrl.php
+++ apps/wap/ctrl/meetCtrl.php
@@ -1,10 +1,15 @@
 <?php
 namespace apps\wap\ctrl;
 use core\lib\conf;
+use apps\wap\model\service;
 class meetCtrl extends baseCtrl{
+  public $sid;
+  public $sdb;
   // 构造方法
-  public function _auto(){
-
+  public function _auto()
+  {
+    $this->sid = isset($_GET['sid']) ? intval($_GET['sid']) : 0;
+    $this->sdb = new service();
   }
 
   /**
@@ -17,16 +22,16 @@ class meetCtrl extends baseCtrl{
       $this->display('meet','index.html');
       die;
     }
-    
+
   }
    public function order(){
- 
+
       // display
       $this->display('meet','order.html');
 
   }
   public function payment(){
- 
+
       // display
       $this->display('meet','payment.html');
 
