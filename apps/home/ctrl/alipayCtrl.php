1d55e49f774224335cfc3883faa365426f601d64
zhaojiangetcunjicom <zhaojian@getcunji.com>
2017-11-16 17:26:12 +0800

    zhaojian

--- apps/home/ctrl/alipayCtrl.php
+++ apps/home/ctrl/alipayCtrl.php
@@ -170,4 +170,94 @@ class alipayCtrl extends \core\icunji{
     }
   }
 
+  /**
+   * 微信支付
+   */
+  public function wxPay(){
+    // Get
+    if (IS_GET === true) {
+
+      //商户订单号，商户网站订单系统中唯一订单号，必填
+      $out_trade_no = createIn($_SESSION['homeUserinfo']['id']);
+
+      //订单名称，必填
+      $subject = '听娱神游约玩账户充值';
+
+      //付款金额，必填
+      $total_amount = bcmul($this->m, 100, 0);
+
+      // 统一下单
+      $jsApiParameters = wxJsapiPay('',$subject,$out_trade_no,$total_amount,$_SESSION['homeUserinfo']['id']);
+      echo J($jsApiParameters);
+      die;
+
+    }
+  }
+
+  // 微信支付回调
+  public function notify(){
+
+    //$xml = $GLOBALS['HTTP_RAW_POST_DATA'];
+    $xml = file_get_contents("php://input");
+
+    // 这句file_put_contents是用来查看服务器返回的XML数据 测试完可以删除了
+    file_put_contents(ICUNJI."/vendor/wxpay/wxlogs/check.log",$xml.PHP_EOL,FILE_APPEND);
+
+    //将服务器返回的XML数据转化为数组
+    //$data = json_decode(json_encode(simplexml_load_string($xml,'SimpleXMLElement',LIBXML_NOCDATA)),true);
+    $data = XA($xml);
+    // 保存微信服务器返回的签名sign
+    $data_sign = $data['sign'];
+    // sign不参与签名算法
+    unset($data['sign']);
+    $sign = makeSign($data);
+
+    // 判断签名是否正确  判断支付状态
+    if ( ($sign===$data_sign) && ($data['return_code']=='SUCCESS') && ($data['result_code']=='SUCCESS') ) {
+        $result = $data;
+        // 这句file_put_contents是用来查看服务器返回的XML数据 测试完可以删除了
+        file_put_contents(ICUNJI."/vendor/wxpay/wxlogs/ok.log",$xml.PHP_EOL,FILE_APPEND);
+
+        //获取服务器返回的数据
+        $out_trade_no = $data['out_trade_no'];  //订单单号
+        $uid = $data['attach'];        //附加参数,选择传递用户uid
+        $openid = $data['openid'];          //付款人openID
+        $total_amount = $data['total_fee'];    //付款金额
+
+        // 微信支付成功后修改订单状态，订单存在则不执行
+        if (!$this->cvdb->getCrow($out_trade_no)) {
+          // 组装数据写入充值表
+          $data['uid'] = $uid;
+          $data['inumber'] = $out_trade_no;
+          $data['money'] = $total_amount;
+          $data['ctime'] = time();
+          $data['type'] = 0;
+          $data['status'] = 2;
+          if ($this->cvdb->add($data)) {
+            // 累加约玩币，约玩值
+            $balance = bcmul($total_amount, conf::get('TEST_BALANCE','alipay'), 0);
+            $contribution = bcmul($total_amount, conf::get('TEST_CONTRIBUTION','alipay'), 0);
+            $uData = $this->udb->getRow($uid);
+            $balance = bcadd($uData['balance'], $balance, 0);
+            $contribution = bcadd($uData['contribution'], $contribution, 0);
+            $upUdata['balance'] = $balance;
+            $upUdata['contribution'] = $contribution;
+            $this->udb->save($uid,$upUdata);
+          }
+        }
+        $result = true;
+    }else{
+        $result = false;
+    }
+    // 返回状态给微信服务器
+    if ($result) {
+        $str='<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>';
+    }else{
+        $str='<xml><return_code><![CDATA[FAIL]]></return_code><return_msg><![CDATA[签名失败]]></return_msg></xml>';
+    }
+    echo $str;
+    return $result;
+
+  }
+
 }
